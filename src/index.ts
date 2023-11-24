f764aa44bce98ea7ffef31913b0ed6b1d5919ea1
Volodymyr Sergeyev <volodymyr.sergeyev@gmail.com>
2023-11-24 12:41:28 +0000

    Updating build process

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { default as NaviExpress } from './NaviExpress';
+
+// Exporting additional types
+export * from './types/types';
+export * from './types/Bag';
+export * from './types/ResJson';
+export * from './types/Controller';
+export * from './types/events/Event';
+export * from './types/events/ReqEvent';
+export * from './types/events/ResEvent';
+export * from './types/events/ErrEvent';
+export * from './types/handlers/ReqHandler';
+export * from './types/handlers/ResHandler';
+export * from './types/handlers/ErrHandler';
