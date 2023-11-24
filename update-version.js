140ea21339b9a02dabcf5141ad1f6f2f24f9797f
Volodymyr Sergeyev <volodymyr.sergeyev@gmail.com>
2023-11-24 00:03:26 +0000

    Publishing as organization public package + removing update-version.js

--- update-version.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const packageJsonPath = './package.json';
-const packageJson = require(packageJsonPath);
-const versionComponents = packageJson.version.split('.').map(num => parseInt(num, 10));
-versionComponents[2] += 1;
-packageJson.version = versionComponents.join('.');
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
-console.log(`Updated version to ${packageJson.version}`);
