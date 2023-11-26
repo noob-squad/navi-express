75f52273a3575bbda3280809740834eafdb15293
Volodymyr Sergeyev <volodymyr.sergeyev@gmail.com>
2023-11-26 22:01:43 +0000

    Exporting additional types as class definition rather then type aliases

---
+++ .changeset/rotten-tools-switch.md
@@ -0,0 +1,5 @@
+---
+"@noob-squad/navi-express": patch
+---
+
+Exporting additional types as class definition rather then type aliases.
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ export { default as NaviExpress } from './NaviExpress';
 
 // Exporting additional types
 export * from './types/types';
-export * from './types/Bag';
-export * from './types/ResJson';
-export * from './types/Controller';
-export * from './types/events/Event';
-export * from './types/events/ReqEvent';
-export * from './types/events/ResEvent';
-export * from './types/events/ErrEvent';
-export * from './types/handlers/ReqHandler';
-export * from './types/handlers/ResHandler';
-export * from './types/handlers/ErrHandler';
+export { default as Bag } from './types/Bag';
+export { default as ResJson } from './types/ResJson';
+export { default as Controller } from './types/Controller';
+export { default as Event } from './types/events/Event';
+export { default as ReqEvent } from './types/events/ReqEvent';
+export { default as ResEvent } from './types/events/ResEvent';
+export { default as ErrEvent } from './types/events/ErrEvent';
+export { default as ReqHandler } from './types/handlers/ReqHandler';
+export { default as ResHandler } from './types/handlers/ResHandler';
+export { default as ErrHandler } from './types/handlers/ErrHandler';
